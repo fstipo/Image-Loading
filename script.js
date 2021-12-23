775147774089feb66f27b1381c9c015c70c972bf
fs <fstipanov@gmail.com>
2021-12-23 15:27:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 'use strict';
+
+// style
+
+const spanEl = document.querySelector('div.pickMe');
+console.log(spanEl);
+console.dir(spanEl.children.length);
+console.dir(spanEl.childNodes.length);
+
+console.log(spanEl.nextElementSibling);
+console.log(spanEl.nextSibling);
+
+console.log(spanEl.parentElement);
+console.log(spanEl.parentNode);
+
+console.log(spanEl.previousElementSibling);
+console.log(spanEl.previousSibling);
