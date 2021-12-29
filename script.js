125b04eac00f13c149342d0f182bec8d9f1a3fee
fs <fstipanov@gmail.com>
2021-12-29 12:05:21 +0100

    add item

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const mainList = document.querySelector('ul');
 const listEl = document.querySelectorAll('li');
 const inputEl = document.querySelector('input');
 
+const addItem = (e) => {
+  if (e.key === 'Enter' && inputEl.value.length > 0) {
+    let elList = [];
+    let liEl = document.createElement('li');
+    liEl.textContent = inputEl.value;
+    mainList.appendChild(liEl);
+    inputEl.value = '';
+    elList = [...listEl, liEl];
+    console.log(elList);
+  }
+};
+
+inputEl.addEventListener('keypress', addItem);
+
 listEl.forEach((el) => {
   el.addEventListener('click', () => {
     let red = el.classList.toggle('red');
