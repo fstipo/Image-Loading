ad2881a573300f58ef47f2ed88445b7e067ba737
fstipo <fstipanov@gmail.com>
2022-01-07 11:04:13 +0100

    JS timer

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const backgroundEl = document.querySelector('.bg-img');
+let countUp = document.querySelector('.countUp');
+
+let countStart = 0;
+let countEnd = 100;
+
+const counter = setInterval(() => {
+  countUp.innerHTML = countStart += 1;
+  let countNow = Number(countUp.innerHTML);
+
+  if (countNow === countEnd) {
+    clearInterval(counter);
+  }
+}, 30);
+
+counter;
