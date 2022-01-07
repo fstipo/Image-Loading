5e846616c6621bfd66c8a61f6ab9e9dfdb657b85
fstipo <fstipanov@gmail.com>
2022-01-07 11:55:02 +0100

    blur counter

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const backgroundEl = document.querySelector('.bg-img');
-let countUp = document.querySelector('.countUp');
+const counterEl = document.querySelector('.countUp');
 
 let countStart = 0;
-let countEnd = 100;
+const countEnd = '100%';
+let blurImg = 70;
 
 const counter = setInterval(() => {
-  countUp.innerHTML = countStart += 1;
-  let countNow = Number(countUp.innerHTML);
+  counterEl.innerHTML = `${(countStart += 1)}%`;
+  let countNow = counterEl.innerHTML;
+  blurImg -= Number(0.6955);
+  backgroundEl.style.filter = `blur(${blurImg.toFixed(0)}px)`;
 
   if (countNow === countEnd) {
     clearInterval(counter);
   }
-}, 30);
+}, 100);
 
 counter;
